81a744365cfcd762f6067005f852169cbf784675
maa7220 <123408096+maa7220@users.noreply.github.com>
2023-02-06 15:30:11 +0200

    Update settings.py

--- icu/settings.py
+++ icu/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&u&^5z72#hyhm$4fm@zba*7*6%k&qpy-@l*l0##9&$@e(8h89*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
